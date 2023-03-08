7c94619e9b3a67603fc0571bd302109941cc0fd8
MelinaLisboa <melinalisboa@gmail.com>
2023-03-08 14:45:02 -0300

    Inclui e exibe negociações na tabela.

--- client/app/domain/negociacao/Negociacoes.js
+++ client/app/domain/negociacao/Negociacoes.js
@@ -1,7 +1,22 @@
 class Negociacoes {
-
   constructor() {
     this._negociacoes = []
+  }
+
+  get volumeTotal() {
+    if (this._negociacoes.length > 0) {
+      return this._negociacoes.reduce((acc, item) => {
+        return acc + item.volume
+      }, 0)
+    }
+
+    // let total = 0
+
+    // for(let i = 0; i < this._negociacoes.length; i++){
+    //   total += this._negociacoes[i].volume
+    // }
+
+    // return total
   }
 
   adiciona(negociacao) {
